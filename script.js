3d792bfe7f0d8396d4ca8ca0ea57313a56758c92
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-08-15 12:33:44 +0300

    starging file

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-// referente type
-var object1 ={value:10};
-var object2 = object1;
-var object3={value:10};
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.querySelector('button');
 
-// content vs scope
-const object4 ={
-	a:function(){
-		console.log(this);
-	}
+function setGradient(){
+	body.style.background =
+	"linear-gradient(to right, " +color1.value
+	+"," + color2.value+")";
+
+	css.textContent = body.style.background + ";";
 }
 
-// instantiation
 
-class PLayer{
-	constructor(name,type) {
-		console.log(this);
-		this.name =name;
-		this.type= type;
-	}
-	introduce() {
-		console.log(`Hi i am ${this.name},I'm ${this.type}`)
-	}
-}
+color1.addEventListener("input", setGradient);
+	
+color2.addEventListener("input", setGradient);
 
-class Wizard extends Player{
-	constructor(name,type) {
-		super(name,type)
-	}
-	play(){
-		console.log(`WEEEE i'm a ${this.type}`);
-	}
-}
 
-const wizard1 = new Wizard('Shelly','Healer');
-const wizard2 = new Wizard('Sean','Dark magic');
+randomColor = () => {
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+};
+
+
+random.addEventListener('click', () => {
+	color1.value = randomColor();
+	color2.value = randomColor();
+	setGradient();
+});
